a91ffe8e1a68d5fb5fb86705574ffe8a076f7b58
unknown <macrowell@yahoo.com>
2022-06-14 18:03:19 -0400

    set up endpoint for get by id

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,15 @@
+const Cars = require('./cars-model');
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  Cars.getById(req.params.id)
+  .then(car => {
+    if(!car) {res.status(404).json({ message: "there are no cars with that id" })}
+    if(car) { 
+      req.car = car
+      next()
+     }
+  })
+  .catch(err => res.json({ message: err.message }))
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +22,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,17 +1,23 @@
 const express = require('express');
 
+const Cars = require('./cars-model');
+
+const md = require('./cars-middleware');
+
 const router = express.Router();
 
 router.get( '/', (req, res) => {
-    res.json({ message: "get router working" })
-})
+    Cars.getAll()
+    .then(cars => res.json(cars))
+    .catch(err => res.status(500).json(err.message))
+});
 
-router.get('/:id', (req, res) => {
-    res.json({ message: "get by id working" })
-})
+router.get('/:id', md.checkCarId, (req, res) => {
+    res.json(req.car)
+});
 
 router.post('/', (req, res) => {
     res.json({ message: "post router is working" })
-})
+});
 
 module.exports = router
