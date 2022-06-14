2276c133811816deab9532b47bb2d3ca5e582359
unknown <macrowell@yahoo.com>
2022-06-14 17:39:37 -0400

    set up endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,4 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get( '/', (req, res) => {
+    res.json({ message: "get router working" })
+})
+
+router.get('/:id', (req, res) => {
+    res.json({ message: "get by id working" })
+})
+
+router.post('/', (req, res) => {
+    res.json({ message: "post router is working" })
+})
+
 module.exports = router
