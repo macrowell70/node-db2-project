c8d9d290e6ac0155bd487547afa7015cf01e8456
unknown <macrowell@yahoo.com>
2022-06-14 17:33:14 -0400

    wired server and router

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('vin').notNullable().unique();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.integer('mileage').notNullable();
+    tbl.string('title').notNullable();
+    tbl.string('transmission').notNullable();
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
